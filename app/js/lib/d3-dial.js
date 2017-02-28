de248a2a697671dd6c803b08f90d3c1364f1f686
Dave Ashman <dave.ashman@uberima.com>
2017-02-28 11:19:47 +0000

    Restraining of mouse movement added and code tidy

--- app/js/lib/d3-dial.js
+++ app/js/lib/d3-dial.js
@@ -11,7 +11,7 @@ rivets.bind(
 
 // var startAngle = -130;
 // var endAngle = startAngle + 260; 
-var DIAL = function(options) {
+var DIAL = function (options) {
 
     var _this = this,
         valueArc,
@@ -38,7 +38,7 @@ var DIAL = function(options) {
 
     var radius = Math.min(this.options.width, this.options.height) / 2;
 
-    this.setData = function() {
+    this.setData = function () {
 
         var datum = []
         var size = this.options.numDegrees / this.options.range;
@@ -57,56 +57,59 @@ var DIAL = function(options) {
         return datum;
     }
 
-    this.initDial = function() {
+    this.initDial = function () {
 
         this.dataSet = this.setData();
 
-        var svg = d3.select(this.options.selector)
+        ////////////////////////////////////////////
+        // BASE SVG START
+        ////////////////////////////////////////////
+        this.svg = d3.select(this.options.selector)
             .append('svg')
             .attr('width', this.options.width)
             .attr('height', this.options.height)
             .attr('class', 'circle-segments')
             .append('g')
             .attr('transform', 'translate(' + (this.options.width / 2) + ',' + (this.options.height / 2) + ')');
-
-        var arc = d3.arc()
+        // END//////////////////////////////////
+        this.baseArc = d3.arc()
             .innerRadius(radius - this.options.donutWidth) // UPDATED
             .outerRadius(radius)
 
 
         var pie = d3.pie()
-            .padAngle(this.options.piePadding)
-            .value(function(d) {
+            // .padAngle(this.options.piePadding)
+            .value(function (d) {
                 return d.count;
             })
             .startAngle(this.options.startAngle * (Math.PI / 180))
             .endAngle(this.options.endAngle * (Math.PI / 180))
             .sort(null);
 
-        var path = svg.selectAll('path')
+        var path = this.svg.selectAll('path')
             .data(pie(this.dataSet))
             .enter()
             .append('path')
-            .attr("data-item-number", function(d) {
+            .attr('stroke', 'white')
+            .attr('stroke-width', 2)
+            .attr("data-item-number", function (d) {
                 return d.data.itemNumber;
             })
-            .attr('d', arc)
-            .attr('class', function(d, i) {
+            .attr('d', this.baseArc)
+            .attr('class', function (d, i) {
                 return 'pie-segment ' + d.data.class
             })
-            .attr('fill', function(d, i) {
+            .attr('fill', function (d, i) {
                 return d.data.fill;
-           }).on('click', function(d, i) {
-
+            }).on('click', function (d, i) {
                 _this.setValue(this, d, i)
-
                 if (_this.options.type == 'drag-range') {
                     _this.setArcEndAngle(_this.valueArc, (d.endAngle - _this.options.piePadding) / (Math.PI / 180), _this.options.startAngle);
                 }
 
-            }).on("mouseover", function(d) {
-                console.log(d)
-            }).each(function(d, i) {
+            }).on("mouseover", function (d) {
+
+            }).each(function (d, i) {
                 if (i === currentState.currValue) {
                     if (_this.options.type !== 'drag-range') {
                         _this.fillSegments(this, i)
@@ -114,60 +117,9 @@ var DIAL = function(options) {
 
                     }
                 }
-
             });
 
-
         if (this.options.type === 'drag-range') {
-
-            var drag = d3.drag()
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on('end', dragended)
-
-            function dragstarted(d) {
-                d3.select(this).raise().classed('dragging', true);
-            }
-            var position = [0, 0];
-            var d;
-
-            function dragged(d) {
-                var halfDW = _this.options.donutWidth;
-                d3.event.sourceEvent.stopPropagation();
-                d_from_origin = Math.sqrt(Math.pow(d3.event.x, 2) + Math.pow(d3.event.y, 2));
-
-                var xy = d3.mouse(_this.handleArc.node());
-                var radians = Math.atan2(xy[0], -xy[1]);
-                var alpha = Math.acos(d3.event.x / d_from_origin);
-
-                _this.valueArc.transition()
-                    .duration(0)
-                    .attrTween("d", arcTween(radians));
-
-                d3.select(this)
-                    .attr("cx", d.x = (radius - (halfDW / 2)) * Math.cos(alpha))
-                    .attr("cy", d.y = d3.event.y < 0 ? -(radius - (halfDW / 2)) * Math.sin(alpha) : (radius - (halfDW / 2)) * Math.sin(alpha));
-
-            }
-
-            function dragended(d, i) {
-                d3.select(this).classed('dragging', false);
-            }
-
-            function angle(cx, cy, ex, ey) {
-                var dy = ey - cy;
-                var dx = ex - cx;
-                var theta = Math.atan2(dy, dx); // range (-PI, PI]
-                theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
-
-                return theta;
-            }
-
-            function angle360(cx, cy, ex, ey) {
-                var theta = angle(cx, cy, ex, ey); // range (-180, 180]
-                if (theta < 0) theta = 360 + theta; // range [0, 360)
-                return theta;
-            }
 
             ////////////////////////////////////////////
             // ARC START
@@ -176,30 +128,25 @@ var DIAL = function(options) {
                 .innerRadius(radius - this.options.donutWidth + 2)
                 .outerRadius(radius - 2)
 
-            this.valueArc = svg.append('path').datum({
-                    startAngle: _this.options.startAngle * (Math.PI / 180),
-                    endAngle: _this.options.startAngle * (Math.PI / 180)
-                })
-                .style("fill", "orange")
+            this.valueArc = this.svg.append('path').datum({
+                startAngle: _this.options.startAngle * (Math.PI / 180),
+                endAngle: _this.options.startAngle * (Math.PI / 180)
+            }).style("fill", "orange")
                 .attr('id', 'value-arc')
                 .attr('stroke', 'orange')
                 .attr('stroke-width', 4 - this.options.piePadding)
                 .attr("d", this.arc)
-                .each(function(d) { this._current = d; });
+                .each(function (d) { this._current = d; });
 
             _this.setArcEndAngle(this.valueArc, _this.options.endAngle - 100, _this.options.startAngle);
-
-            ////////////////////////////////////////////
-            // ARC END
-            ////////////////////////////////////////////
+            // END//////////////////////////////////
 
             ////////////////////////////////////////////
             // HANDLE START
             ////////////////////////////////////////////
-
-            this.handleArc = svg.append('circle')
+            this.handleArc = this.svg.append('circle')
                 .attr('r', radius - this.options.donutWidth)
-                .attr('stroke', 'black')
+                .attr('stroke', 'orange')
                 .attr('strokeWidth', 20)
                 .attr('fill', 'none')
                 .attr('class', 'circumference');
@@ -208,42 +155,83 @@ var DIAL = function(options) {
                 x: 0,
                 y: -radius + (_this.options.donutWidth / 2)
             }];
-          
-            this.handle = svg.append("g")
+
+            this.handle = this.svg.append("g")
                 .attr("class", "dot")
                 .selectAll('circle')
                 .data(this.handleData)
                 .enter().append("circle")
+                .attr('fill', 'black')
                 .attr("r", this.options.donutWidth / 2)
-                .attr("cx", function(d) { return d.x; })
-                .attr("cy", function(d) { return d.y; })
+                .attr("cx", function (d) { return d.x; })
+                .attr("cy", function (d) { return d.y; })
                 .call(drag);
-
-
-
-
-
-
-            ////////////////////////////////////////////
-            // HANDLE END
-            ////////////////////////////////////////////
-
-
+            // END//////////////////////////////////
         }
 
     }
 
-    this.getHandleOffset = function() {
-        return radius + (_this.options.donutWidth / 2)
+    ////////////////////////////////////////////
+    // DRAG EVENTS START
+    ////////////////////////////////////////////
+    var drag = d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on('end', dragended)
+
+    /**  Drag start event
+     ------------------------------------------------------*/
+    function dragstarted(d) {
+        d3.select(this).raise().classed('dragging', true);
     }
 
-    this.setValue = function(element, data, value) {
-        // var data = d.data;
-        this.toggleSelected(element, data, value)
+    /** Dragging event
+     ------------------------------------------------------*/
+    function dragged(d) {
+
+        var halfDW = _this.options.donutWidth;
+        d3.event.sourceEvent.stopPropagation();
+        var d_from_origin = Math.sqrt(Math.pow(d3.event.x, 2) + Math.pow(d3.event.y, 2));
+
+        // XY Thanks to http://jsfiddle.net/eremita/PSLMR/
+        var xy = d3.mouse(_this.handleArc.node());
+        var radians = Math.atan2(xy[0], -xy[1]);
+        var alpha = Math.acos(d3.event.x / d_from_origin);
+
+        var currentAngle = radians * (180 / Math.PI);
+
+        if (currentAngle < _this.options.startAngle || currentAngle > _this.options.endAngle || d_from_origin < 45) {
+            return
+        }
+
+        _this.valueArc.transition()
+            .duration(0)
+            .attrTween("d", _this.arcTween(radians));
+
+        d3.select(this)
+            .attr("cx", d.x = (radius - (halfDW / 2)) * Math.cos(alpha))
+            .attr("cy", d.y = d3.event.y < 0 ? -(radius - (halfDW / 2)) * Math.sin(alpha) : (radius - (halfDW / 2)) * Math.sin(alpha));
+
+    }
+    /** Drag end event
+     ------------------------------------------------------*/
+    function dragended(d, i) {
+        d3.select(this).classed('dragging', false);
+    }
+    // END//////////////////////////////////
+
+
+    this.setValue = function (element, data, value) {
+        this.setSelected(element, data, value)
     }
 
+
+    this.setSelected = function (element, data, value) {
+        this.fillSegments(element, value)
+        currentState.currValue = value;
+    }
     // Fills segments based on selected value
-    this.fillSegments = function(element, value, data) {
+    this.fillSegments = function (element, value, data) {
 
         switch (this.options.type) {
             case 'range-select':
@@ -252,82 +240,38 @@ var DIAL = function(options) {
             case 'single-select':
                 this.selectSingleSegment(element, value);
                 break;
-            case 'drag-range':
-                // this.setArcEndAngle(this.valueArc, value, data.startAngle);
-                break;
         }
     }
 
-    // Returns a tween for a transition’s "d" attribute, transitioning any selected
-    // arcs from their current angle to the specified new angle.
-    function arcTween(newAngle) {
-
-        // The function passed to attrTween is invoked for each selected element when
-        // the transition starts, and for each element returns the interpolator to use
-        // over the course of transition. This function is thus responsible for
-        // determining the starting angle of the transition (which is pulled from the
-        // element’s bound datum, d.endAngle), and the ending angle (simply the
-        // newAngle argument to the enclosing function).
-        return function(d) {
-
-            // To interpolate between the two angles, we use the default d3.interpolate.
-            // (Internally, this maps to d3.interpolateNumber, since both of the
-            // arguments to d3.interpolate are numbers.) The returned function takes a
-            // single argument t and returns a number between the starting angle and the
-            // ending angle. When t = 0, it returns d.endAngle; when t = 1, it returns
-            // newAngle; and for 0 < t < 1 it returns an angle in-between.
+    this.arcTween = function(newAngle) {
+        return function (d) {
             var interpolate = d3.interpolate(d.endAngle, newAngle);
 
-            // The return value of the attrTween is also a function: the function that
-            // we want to run for each tick of the transition. Because we used
-            // attrTween("d"), the return value of this last function will be set to the
-            // "d" attribute at every tick. (It’s also possible to use transition.tween
-            // to run arbitrary code for every tick, say if you want to set multiple
-            // attributes from a single function.) The argument t ranges from 0, at the
-            // start of the transition, to 1, at the end.
-            return function(t) {
+            return function (t) {
 
-                // Calculate the current arc angle based on the transition time, t. Since
-                // the t for the transition and the t for the interpolate both range from
-                // 0 to 1, we can pass t directly to the interpolator.
-                //
-                // Note that the interpolated angle is written into the element’s bound
-                // data object! This is important: it means that if the transition were
-                // interrupted, the data bound to the element would still be consistent
-                // with its appearance. Whenever we start a new arc transition, the
-                // correct starting angle can be inferred from the data.
                 d.endAngle = interpolate(t);
-
-                // Lastly, compute the arc path given the updated data! In effect, this
-                // transition uses data-space interpolation: the data is interpolated
-                // (that is, the end angle) rather than the path string itself.
-                // Interpolating the angles in polar coordinates, rather than the raw path
-                // string, produces valid intermediate arcs during the transition.
                 return _this.arc(d);
             };
         };
     }
-    this.setArcEndAngle = function(arc, endAngle) {
 
+    this.setArcEndAngle = function (arc, endAngle) {
         endAngle = endAngle * (Math.PI / 180);
-
-        console.log('endAngle is', endAngle)
-
         arc.transition()
             .duration(250)
-            .attrTween("d", arcTween(endAngle));
+            .attrTween("d", _this.arcTween(endAngle));
     }
 
-    this.selectSingleSegment = function(element, value) {
+    this.selectSingleSegment = function (element, value) {
         var allSegments = element.parentNode.childNodes;
-        allSegments.forEach(function(el) {
+        allSegments.forEach(function (el) {
             d3.select(el).classed('selected', false);
         });
         d3.select(element).classed('selected', true);
     }
 
-    this.fillRange = function(element, value) {
-        // console.log(value)
+    this.fillRange = function (element, value) {
+
         var allSegments = element.parentNode.childNodes;
         var numSegments = allSegments.length;
         var dir = 'increase';
@@ -338,7 +282,7 @@ var DIAL = function(options) {
         if (dir === 'increase') {
             var index = 0;
             for (var i = 0; i < numSegments; i++) {
-                setTimeout(function() {
+                setTimeout(function () {
                     if (index <= value) {
                         d3.select(allSegments[index]).classed('selected', true);
                     } else {
@@ -350,20 +294,12 @@ var DIAL = function(options) {
         } else {
             var index = currentState.currValue;
             for (var i = currentState.currValue; i > value; i--) {
-                setTimeout(function() {
+                setTimeout(function () {
                     d3.select(allSegments[index]).classed('selected', false);
                     --index;
                 }, i * (numSegments / 25));
             }
         }
-    }
-
-
-
-    this.toggleSelected = function(element, data, value) {
-        this.fillSegments(element, value)
-        currentState.currValue = value;
-        data.isSelected = !data.isSelected;
     }
 
     this.initDial();
@@ -372,7 +308,7 @@ var DIAL = function(options) {
 
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     var dial = new DIAL({
         type: 'drag-range',
         startAngle: -130,
@@ -389,8 +325,8 @@ $(document).ready(function() {
         numDegrees: 260,
         range: 10,
         selector: '#dial2',
-        height: 150,
-        width: 150
+        height: 400,
+        width: 400
     });
 
     var dial3 = new DIAL({
@@ -399,8 +335,8 @@ $(document).ready(function() {
         numDegrees: 260,
         range: 12,
         selector: '#dial3',
-        height: 150,
-        width: 150
+        height: 400,
+        width: 400
     });
 
 })
